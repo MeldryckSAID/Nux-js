3309e7cdf463001cf66f44f96545fd49b2b47a43
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-11-01 08:43:47 +0100

    feat newsletter

---
+++ components/section/MyServicePrismics.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="service" v-for="item in services_card">
+    <img :src="item.service_image_card.url" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  Cardservice: Array,
+});
+</script>
+<style lang="scss" scooped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,10 +15,32 @@ const { data: home } = await useAsyncData("home", () =>
 
     <MyInformationPrismics :info="home.data.info" />
     <!-- <Myinformation /> -->
+    <!-- <pre>{{ home.data }}</pre> -->
 
-    <pre>{{ home.data.newsletter }}</pre>
-    <MynewsletterPrismics :ntitle="home.data.newsletter" />
+    <h5 class="presentation">
+      <PrismicRichText :field="home.data.presentation_service" />
+    </h5>
+    <h5><PrismicRichText :field="home.data.service_title" /></h5>
+
+    <MyServicePrismics :field="home.data.services_card" />
+
+    <pre>{{ home.data.services_card }}</pre>
+
+    <MyCardsPresta />
+
+    <MynewsletterPrismics :newsletter="home.data.newsletter" />
+
+    <!-- <MyNewsletter
+      imageSrc="..//public/burger.png"
+      imageAlt="Image aléchante d'un burger"
+      title="Subcribe To Our Newsletter"
+      paragraphe="Lorem ipsum dolor sit amet, consectetur adipidrscing elit. Purus mauris sem sed urna venenatis vivamus. Egestas in velit nulla viverra turpis id ac. Amet faucibus tempus."
+      /> -->
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.presentation {
+  color: $primary-color;
+}
+</style>
