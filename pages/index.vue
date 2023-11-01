bd76bcb75788b7e05f845e5b16ae61ab1c9d6ce4
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2023-11-01 09:09:34 +0100

    feat ( service)

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ const { data: home } = await useAsyncData("home", () =>
     />
 
     <MyInformationPrismics :info="home.data.info" />
-    <!-- <Myinformation /> -->
+
     <!-- <pre>{{ home.data }}</pre> -->
 
     <h5 class="presentation">
@@ -22,20 +22,11 @@ const { data: home } = await useAsyncData("home", () =>
     </h5>
     <h5><PrismicRichText :field="home.data.service_title" /></h5>
 
-    <MyServicePrismics :field="home.data.services_card" />
+    <MyServicePrismics :Cardservice="home.data.services_card" />
 
-    <pre>{{ home.data.services_card }}</pre>
-
-    <MyCardsPresta />
+    
 
     <MynewsletterPrismics :newsletter="home.data.newsletter" />
-
-    <!-- <MyNewsletter
-      imageSrc="..//public/burger.png"
-      imageAlt="Image aléchante d'un burger"
-      title="Subcribe To Our Newsletter"
-      paragraphe="Lorem ipsum dolor sit amet, consectetur adipidrscing elit. Purus mauris sem sed urna venenatis vivamus. Egestas in velit nulla viverra turpis id ac. Amet faucibus tempus."
-      /> -->
   </div>
 </template>
 
